8137334cb5322bf85458c66da9edbf4ee44c9a55
Brian Vaughn <brianvaughn@brians-mbp.lan>
2020-08-04 12:50:16 -0500

    Initial version

--- ui.frontend/src/app/components/title/title.component.ts
+++ ui.frontend/src/app/components/title/title.component.ts
@@ -1,5 +1,5 @@
 import { MapTo } from '@adobe/cq-angular-editable-components';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, TemplateRef } from '@angular/core';
 
 const TitleEditConfig = {
   emptyLabel: 'Title',
@@ -15,10 +15,28 @@ const TitleEditConfig = {
 export class TitleComponent implements OnInit {
 
   @Input() text: string;
+  @Input() linkURL: string;
+  @Input() linkDisabled: boolean;
+
+  @Input() titleTemplate: TemplateRef<any>;
+
+  @ViewChild('titleOnly')
+  private titleOnlyTpl: TemplateRef<any>;
+
+  @ViewChild('titleLink')
+  private titleLinkTpl: TemplateRef<any>;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.titleTemplate = this.titleOnlyTpl;
+    console.log('titleTemplate-' + this.titleTemplate);
+    console.log('titleOnlyTpl-' + this.titleOnlyTpl);
+    console.log('titleLinkTpl-' + this.titleLinkTpl);
+  }
+
+  get hasLink(): boolean {
+    return this.linkURL && this.linkURL.trim().length > 0 && !this.linkDisabled;
   }
 
 }
