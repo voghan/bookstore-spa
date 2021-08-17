6211482889fd98cec0181a8a7fceb339a5181713
Brian Vaughn <bfvaughn@@gmail.com>
2021-08-17 10:31:10 -0500

    build changes and bootstrap

--- ui.frontend/src/main.ts
+++ ui.frontend/src/main.ts
@@ -26,10 +26,22 @@ if (environment.production) {
 } else {
   // In development mode, redirect from "/" to app root
   if (location.pathname === '/' && environment.appRoot) {
-    // location.href = environment.appRoot;
-    enableProdMode();
+    location.href = environment.appRoot;
   }
 }
+
+const initialStateScriptTag = document.getElementById('__AEM_STATE__');
+if(!!initialStateScriptTag) {
+  try {
+    const initialState = JSON.parse(initialStateScriptTag.innerHTML);
+    // @ts-ignore
+    window.initialModel = initialState.rootModel;
+    initialStateScriptTag.remove();
+  }catch(err){
+    console.warn('failed to hydrate app', err);
+  }
+}
+
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
